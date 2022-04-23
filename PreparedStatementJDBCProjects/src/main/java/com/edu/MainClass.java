5e9a5d5cb6b6aaf94ebcd1f093e17de3676e44a2
SRI VIDHYA C <SRI VIDHYA C@LAPTOP-G9VMJ633>
2022-04-23 13:38:07 +0530

    jdbcstatement

---
+++ PreparedStatementJDBCProjects/src/main/java/com/edu/MainClass.java
@@ -0,0 +1,42 @@
+package com.edu;
+
+import java.util.Scanner;
+
+
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		
+				Scanner sc=new Scanner(System.in);
+				while(true) {
+				System.out.println("Database operations");
+				System.out.println("Enter your choice");
+				System.out.println("1.To display Student information");
+				System.out.println("2.Insert Student record");
+				System.out.println("3.Update Student information based on id");
+				System.out.println("4.Delete Student information based on id");
+				System.out.println("5.Select Student based on ID");
+				int ch=sc.nextInt();
+				
+				switch(ch) {
+				case 1:DataBaseOperations.displayRecords();
+				        break;
+				case 2: DataBaseOperations.addRecords();
+				      break;
+				case 3: DataBaseOperations.updateRecords();
+				 default:System.out.println("Invalid choice");
+				} 
+				
+				System.out.println("Do you want to continue yes/no");
+				String choice=sc.next();
+				if(choice.equalsIgnoreCase("no")) {
+					break;
+				} 
+				
+				}
+				System.out.println("Program Terminated");
+			} 
+
+		}
